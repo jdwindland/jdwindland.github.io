afe6afee8bf5fbd9b2fc81015c56f9492feaea1e
jdwindland <35585941+jdwindland@users.noreply.github.com>
2018-04-18 01:44:59 -0500

    JSON Complete

--- assignments/term-website/scripts/pricerepair.js
+++ assignments/term-website/scripts/pricerepair.js
@@ -1,16 +1,19 @@
-var myJSON = '{"minor":"$50","major":"$85","overhaul":"$90 – $150","drivetrain":"$45","tire":"$5 - $15","brake":"$30/wheel","spoke":"$20 + Parts","assemblenew":"$50 - $150","assembleused":"$25 - $50","box":"$45 - $65"}';
-
-var myObj = JSON.parse(myJSON);
-document.getElementById("price1").innerHTML = myObj.minor;
-document.getElementById("price2").innerHTML = myObj.major;
-document.getElementById("price3").innerHTML = myObj.overhaul;
-document.getElementById("price4").innerHTML = myObj.drivetrain;
-document.getElementById("price5").innerHTML = myObj.tire;
-document.getElementById("price6").innerHTML = myObj.brake;
-document.getElementById("price7").innerHTML = myObj.spoke;
-document.getElementById("price8").innerHTML = myObj.assemblenew;
-document.getElementById("price9").innerHTML = myObj.assembleused;
-document.getElementById("price10").innerHTML = myObj.box;
-
-document.getElementById("price").innerHTML = myObj.box;
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        document.getElementById("price1").innerHTML = myObj.minor;
+        document.getElementById("price2").innerHTML = myObj.major;
+        document.getElementById("price3").innerHTML = myObj.overhaul;
+        document.getElementById("price4").innerHTML = myObj.drivetrain;
+        document.getElementById("price5").innerHTML = myObj.tire;
+        document.getElementById("price6").innerHTML = myObj.brake;
+        document.getElementById("price7").innerHTML = myObj.spoke;
+        document.getElementById("price8").innerHTML = myObj.assemblenew;
+        document.getElementById("price9").innerHTML = myObj.assembleused;
+        document.getElementById("price10").innerHTML = myObj.box;
+    }
+};
+xmlhttp.open("GET", "https://jdwindland.github.io/assignments/term-website/data/repairprice.json", true);
+xmlhttp.send();
 
